f0d7b03a9effdcf8166858dba1486ed18456df43
Olivia Carlisle <151247171+oliviacarlisle@users.noreply.github.com>
2024-10-26 11:47:59 -0400

    Update prototypes.js
    - Add code examples with the instanceof operator
    - Add code to log/show the contents of Bird.prototype

--- src/prototypes.js
+++ src/prototypes.js
@@ -12,7 +12,9 @@ bird.sayHi(); // 'Hi, I am mockingjay'
 
 // what is the prototype property of Bird?
 // - it's just an object! with our sayHi method on it, and the constructor function
-// the constructor method on the prototype is a circular reference that points back to our Bird function!
+console.log(Bird.prototype); // Bird { sayHi: λ, constructor: λ:Bird }
+// - the constructor method on the prototype is a circular reference that points back
+//   to our Bird function!
 console.log(Bird.prototype.constructor === Bird); // true
 
 // but what is the __proto__ of Bird?
@@ -35,3 +37,11 @@ console.log(Object.getPrototypeOf(Bird.prototype) === Object.prototype); // true
 const fn = () => "I am the fn arrow function";
 console.log(fn.prototype); // undefined
 console.log(Object.getPrototypeOf(fn) === Function.prototype); // true
+
+// The instanceof operator tests to see if the prototype property of a constructor
+// appears anywhere in the prototype chain of an object.
+// check that the __proto__ of bird is Bird.prototype
+console.log(Object.getPrototypeOf(bird) === Bird.prototype); // true
+console.log(bird instanceof Bird); // true
+console.log(bird instanceof Object); // true
+console.log(Bird instanceof Object); // true
